e089edccdbcbd887ee50d14827c452a2da582785
yizhan wang <yizhan.wang@booktrack.com>
2016-11-28 19:47:23 +1300

    Initial Commit

---
+++ trump100day/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  trump100day
+//
+//  Created by yizhan wang on 28/11/16.
+//  Copyright © 2016 lambda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
